cd295ff55f7627e34b8629dd7208a1bd077d8832
Ana <anapcardosodias@gmail.com>
2021-04-04 22:31:56 +0100

    changed function

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
+function getForecast(position) {
+  console.log(position);
   let apiKey = "d0e932fc6acbbe94467003adf5e15de0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
