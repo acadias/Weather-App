4ceb2844c4f4ef15738ee0f76d2dfd7d887d17b9
Ana <anapcardosodias@gmail.com>
2021-04-04 21:59:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "d0e932fc6acbbe94467003adf5e15de0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -124,6 +124,8 @@ function showCurrentTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function retrievePosition(position) {
