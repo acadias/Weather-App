7f283b01fa1e665720fbdda7929c086388e62572
Ana <anapcardosodias@gmail.com>
2021-04-04 23:11:27 +0100

    Fixed wrong code in js / style and altered the icons/forecast visual
    Fixed wrong code in js that wasn't giving the right forecast
    Styled the new icons of weather
    Changed the text

--- index.js
+++ index.js
@@ -46,33 +46,31 @@ let h2 = document.querySelector("#date-month-year");
 let h5 = document.querySelector("#time");
 h2.innerHTML = currentDate(now);
 h5.innerHTML = formatHours(now);
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return days[date.getDay()];
+}
 function search(event) {
   event.preventDefault();
   let city = document.querySelector("#write-city");
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${city.value}`;
-
   let apiKey = "d0e932fc6acbbe94467003adf5e15de0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric`;
-
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showCurrentTemperature);
 }
-
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
-
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
         forecastHTML +
-        `
-<div class="col ">
+        `<div class="col">
           <strong class= weather-forecast-date>${formatDay(
-            forecastDay.dt
+            forecastDay.dt * 1000
           )}</strong><br />
           <img class="weekdays-icon" src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
@@ -83,26 +81,23 @@ function displayForecast(response) {
           <div class="row row-cols-1">
             <div class="col" class= weather-forecast-temperature-min>${Math.round(
               forecastDay.temp.max
-            )}°</div>  
+            )} C°</div>  
  <div class="col" class= weather-forecast-temperature-max>${Math.round(
    forecastDay.temp.min
- )}° </div>
+ )} C°</div>
+          </div>
           </div>
   `;
     }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-function getForecast(position) {
-  console.log(position);
+function getForecast(coordinates) {
   let apiKey = "d0e932fc6acbbe94467003adf5e15de0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 function showCurrentTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let windElement = document.querySelector("#wind");
@@ -110,24 +105,19 @@ function showCurrentTemperature(response) {
   let descriptionElement = document.querySelector("h4");
   let h1 = document.querySelector("h1");
   let iconElement = document.querySelector("#icon");
-
   celsiusTemperature = response.data.main.temp;
-
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement = response.data.main.humidity;
   h1.innerHTML = response.data.name;
-
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
   getForecast(response.data.coord);
 }
-
 function retrievePosition(position) {
   console.log(position);
   let latitude = position.coords.latitude;
@@ -137,12 +127,10 @@ function retrievePosition(position) {
   let apiLatUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiLatUrl).then(showCurrentTemperature);
 }
-
 function getPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
-
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -151,7 +139,6 @@ function convertToFahrenheit(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
 function convertToCelsius(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
@@ -159,18 +146,12 @@ function convertToCelsius(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
 let celsiusTemperature = null;
-
 let currentButton = document.querySelector("#current-location");
 currentButton.addEventListener("click", getPosition);
-
 let form = document.querySelector("#search-form");
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 form.addEventListener("submit", search);
